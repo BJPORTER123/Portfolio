b82315e7e5ce1a79541b9aab3001f178c331b509
Ben <bjporter123@gmail.com>
2023-04-23 17:13:03 +0100

    Updates

---
+++ precache-manifest.4c3fe0f96006cd62375e581da7538658.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2deb7fbf2d37cbec766",
+    "url": "/Portfolio/static/css/main.c49b45b1.chunk.css"
+  },
+  {
+    "revision": "e2deb7fbf2d37cbec766",
+    "url": "/Portfolio/static/js/main.e2deb7fb.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/Portfolio/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "fbfb248fc1056946107a",
+    "url": "/Portfolio/static/js/2.fbfb248f.chunk.js"
+  },
+  {
+    "revision": "61fb79696eb1d06d04cb",
+    "url": "/Portfolio/static/js/runtime~main.61fb7969.js"
+  },
+  {
+    "revision": "c1ee91ec6ebe8bbe51d400d1bd48694b",
+    "url": "/Portfolio/static/media/desk.c1ee91ec.png"
+  },
+  {
+    "revision": "a11baf2b57bd9f4e94086ead1528877a",
+    "url": "/Portfolio/static/media/earth.a11baf2b.png"
+  },
+  {
+    "revision": "d7b98c450349bef0631c7229b804a5c7",
+    "url": "/Portfolio/static/media/Minecraft.d7b98c45.ttf"
+  },
+  {
+    "revision": "eb59282e02abf5305bfc36e9d2fd61a4",
+    "url": "/Portfolio/index.html"
+  }
+];
