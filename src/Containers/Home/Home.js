59323875f8e9a5ac4ab08a0049f6a173e54f593f
Ben <bjporter123@gmail.com>
2023-04-23 14:12:35 +0100

    commit

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import NavBar from "../NavBar/NavBar";
 import TypingEffect from "../../Components/TypingEffect";
 import hole from "../../images/hole.png";
 import desk from "../../images/desk.png";
@@ -8,22 +7,22 @@ import "./Home.css";
 
 
 const Home = () => {
-  const handleZoomClick = (event) => {
-    event.preventDefault();
-    const zoomedInDiv = document.getElementById("zoomed-in-div");
-    zoomedInDiv.style.transition = "transform 0.5s ease-in-out";
-    zoomedInDiv.style.transform = "scale(20) rotate(360deg)";
-    zoomedInDiv.style.opacity = 1;
-    const fadeInterval = setInterval(() => {
-      zoomedInDiv.style.opacity -= 0.001;
-    }, 30);
+  // const handleZoomClick = (event) => {
+  //   event.preventDefault();
+  //   const zoomedInDiv = document.getElementById("zoomed-in-div");
+  //   zoomedInDiv.style.transition = "transform 0.5s ease-in-out";
+  //   zoomedInDiv.style.transform = "scale(20) rotate(360deg)";
+  //   zoomedInDiv.style.opacity = 1;
+  //   const fadeInterval = setInterval(() => {
+  //     zoomedInDiv.style.opacity -= 0.001;
+  //   }, 30);
 
-    setTimeout(() => {
-      clearInterval(fadeInterval);
-      zoomedInDiv.style.opacity = 0;
-      window.location.href = event.target.href;
-    }, 300);
-  };
+  //   setTimeout(() => {
+  //     clearInterval(fadeInterval);
+  //     zoomedInDiv.style.opacity = 0;
+  //     window.location.href = event.target.href;
+  //   }, 300);
+  // };
 
   return (
     <div className="home-page-container">
