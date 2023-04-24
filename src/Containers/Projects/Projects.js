d18ed78ea26bc3cda650042406a914309dc42588
Ben <bjporter123@gmail.com>
2023-04-24 16:52:38 +0100

    adding to projects

--- src/Containers/Projects/Projects.js
+++ src/Containers/Projects/Projects.js
@@ -1,5 +1,6 @@
-import './Projects.css'
 import React from 'react';
+import travelApp from '../../images/travelapp.png'
+import './Projects.css'
 
 const Projects = () => {
     return (
@@ -7,12 +8,54 @@ const Projects = () => {
     
         <h2 className='title-project'>Projects </h2>
         <div className="main-container-project">
-            <div className='project-item'></div>
-            <div className='project-item'></div>
-            <div className='project-item'></div>
-            <div className='project-item'></div>
-            <div className='project-item'></div>
-            <div className='project-item'></div>
+            <div className='project-item'>
+              <img className='project-photo' src={travelApp} alt='Travel app Project'/>
+              <section>
+                  <h3>Travel App</h3>
+                  <p>
+                    Travel app project blah blah 
+                  </p>
+              </section>
+              <p>
+                tecnologies used 
+              </p>
+            </div>
+            <div className='project-item'>
+              <img className='project-photo' src={travelApp} alt='Travel app Project'/>
+              <section>
+                  <h3>Travel App</h3>
+                  <p>
+                    Travel app project blah blah 
+                  </p>
+              </section>
+              <p>
+                tecnologies used 
+              </p>
+            </div>
+            <div className='project-item'>
+              <img className='project-photo' src={travelApp} alt='Travel app Project'/>
+              <section>
+                  <h3>Travel App</h3>
+                  <p>
+                    Travel app project blah blah 
+                  </p>
+              </section>
+              <p>
+                tecnologies used 
+              </p>
+            </div>
+            <div className='project-item'>
+              <img className='project-photo' src={travelApp} alt='Travel app Project'/>
+              <section>
+                  <h3>Travel App</h3>
+                  <p>
+                    Travel app project blah blah 
+                  </p>
+              </section>
+              <p>
+                tecnologies used 
+              </p>
+            </div>
         </div>
       </div>
     );
