68946ae225360925bc01a9dfeae946c92de1dcf4
Ben <bjporter123@gmail.com>
2023-04-27 12:58:01 +0100

    css projects

--- src/Containers/Projects/Projects.js
+++ src/Containers/Projects/Projects.js
@@ -6,28 +6,52 @@ const Projects = () => {
     return (
         <div className="main-container-projects">
           <div className='project-item'>
-            <img className='project-photo' src={travelApp} alt='Travel app Project'/>
-          <div className='project-description'>
-            <h3 className='project-title'>Travel App</h3>
-            <p className='contains-description'>
-              Travel app project blah blah 
-            </p>  
-          </div>
-            <div className='contains-tech'>
-              tecnologies used 
+              <img className='project-photo' src={travelApp} alt='Travel app Project'/>
+            <div className='project-description'>
+              <h3 className='project-title'>Travel App</h3>
+              <p className='contains-description'>
+                Travel app project blah blah 
+              </p>  
             </div>
+              <div className='contains-tech'>
+                tecnologies used 
+              </div>
           </div>
           <div className='project-item'>
-            <img className='project-photo' src={travelApp} alt='Travel app Project'/>
-          <div className='project-description'>
-            <h3 className='project-title'>Travel App</h3>
-            <p className='contains-description'>
-              Travel app project blah blah 
-            </p>  
-          </div>
-            <div className='contains-tech'>
-              tecnologies used 
+              <img className='project-photo' src={travelApp} alt='Travel app Project'/>
+            <div className='project-description'>
+              <h3 className='project-title'>Travel App</h3>
+              <p className='contains-description'>
+                Travel app project blah blah 
+              </p>  
             </div>
+              <div className='contains-tech'>
+                tecnologies used 
+              </div>
+          </div>
+          <div className='project-item'>
+              <img className='project-photo' src={travelApp} alt='Travel app Project'/>
+            <div className='project-description'>
+              <h3 className='project-title'>Travel App</h3>
+              <p className='contains-description'>
+                Travel app project blah blah 
+              </p>  
+            </div>
+              <div className='contains-tech'>
+                tecnologies used 
+              </div>
+          </div>
+          <div className='project-item'>
+              <img className='project-photo' src={travelApp} alt='Travel app Project'/>
+            <div className='project-description'>
+              <h3 className='project-title'>Travel App</h3>
+              <p className='contains-description'>
+                Travel app project blah blah 
+              </p>  
+            </div>
+              <div className='contains-tech'>
+                tecnologies used 
+              </div>
           </div>
       </div>
     );
