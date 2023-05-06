8cbd8976ffdc065d137a1fccfa71143b63178978
Ben <bjporter123@gmail.com>
2023-05-06 18:39:22 +0100

    coommit

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -3,6 +3,10 @@ import React from "react";
 const Contact = () => {
     return (  
         <>
+        <div className="container">
+            <h2>Contact</h2>
+
+        </div>
         </>
     );
 }
