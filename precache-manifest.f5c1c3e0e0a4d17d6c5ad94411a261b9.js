859d1e89889b4dac49a945058dc5b49c043ef7de
Ben <bjporter123@gmail.com>
2023-05-08 17:25:53 +0100

    Updates

---
+++ precache-manifest.f5c1c3e0e0a4d17d6c5ad94411a261b9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "377f2cdcce0c932413c7",
+    "url": "/Portfolio/static/css/main.1b18b325.chunk.css"
+  },
+  {
+    "revision": "377f2cdcce0c932413c7",
+    "url": "/Portfolio/static/js/main.377f2cdc.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/Portfolio/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "ea32d3c07511d9115f74",
+    "url": "/Portfolio/static/js/2.ea32d3c0.chunk.js"
+  },
+  {
+    "revision": "61fb79696eb1d06d04cb",
+    "url": "/Portfolio/static/js/runtime~main.61fb7969.js"
+  },
+  {
+    "revision": "da4579c7295c62559f87415d5d5cc2b9",
+    "url": "/Portfolio/static/media/profilephoto.da4579c7.jpg"
+  },
+  {
+    "revision": "015641121dfdc259ba7112f1577901e7",
+    "url": "/Portfolio/static/media/travelapp.01564112.png"
+  },
+  {
+    "revision": "eb8d3f7a045914d2adc49f6d94fa6579",
+    "url": "/Portfolio/static/media/Robit.eb8d3f7a.png"
+  },
+  {
+    "revision": "ad485cc7210355edb20506747e068c67",
+    "url": "/Portfolio/static/media/triviaapp.ad485cc7.png"
+  },
+  {
+    "revision": "bd5770217f05eba8a2fc4b7ca8520ce5",
+    "url": "/Portfolio/static/media/world-spin.bd577021.gif"
+  },
+  {
+    "revision": "d7b98c450349bef0631c7229b804a5c7",
+    "url": "/Portfolio/static/media/Minecraft.d7b98c45.ttf"
+  },
+  {
+    "revision": "8e502ecdbc4c12016b405ac41e53a737",
+    "url": "/Portfolio/static/media/sc-fi-texturing.8e502ecd.png"
+  },
+  {
+    "revision": "ef8760ae74f6dfe41d95e00434c24e49",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.4c3fe0f96006cd62375e581da7538658.js"
+  "/Portfolio/precache-manifest.f5c1c3e0e0a4d17d6c5ad94411a261b9.js"
 );
 
 workbox.clientsClaim();
